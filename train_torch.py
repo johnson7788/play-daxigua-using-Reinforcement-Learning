4dec955c58d10ffb5758d006a8e37f390a2d0026
johnson <johnson@github.com>
2021-05-10 13:54:43 +0800

    2021-05-10_13:54

--- train_torch.py
+++ train_torch.py
@@ -18,10 +18,10 @@ INITIAL_EPSILON = 0.0001  # starting value of epsilon
 FINAL_EPSILON = 0.0001  # final value of epsilon
 # 以前要记住的transitions的数量, 保留的最大的buffer记忆的数量
 REPLAY_MEMORY = 2000
-#训练的batch_size
+# 训练的batch_size
 BATCH_SIZE = 8
 FRAME_PER_ACTION = 1
-#每多少个时间步更新下Target Q网络
+# 每多少个时间步更新下Target Q网络
 UPDATE_TIME = 100
 # 游戏观察的画面进行resize，特征的大小
 width = 80
@@ -45,11 +45,11 @@ def preprocess(observation):
 
 
 class DeepNetWork(nn.Module):
-    def __init__(self,):
+    def __init__(self, ):
         """
         三个卷积+全连接
         """
-        super(DeepNetWork,self).__init__()
+        super(DeepNetWork, self).__init__()
         self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels=4, out_channels=32, kernel_size=8, stride=4, padding=2),
             nn.ReLU(inplace=True),
@@ -64,20 +64,22 @@ class DeepNetWork(nn.Module):
             nn.ReLU(inplace=True)
         )
         self.fc1 = nn.Sequential(
-            nn.Linear(1600,256),
+            nn.Linear(1600, 256),
             nn.ReLU()
         )
-        #输出的动作数应该是离散的动作数
-        self.out = nn.Linear(256,14)
+        # 输出的动作数应该是离散的动作数
+        self.out = nn.Linear(256, 14)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
-        x = x.view(x.size(0),-1)
+        x = x.view(x.size(0), -1)
         x = self.fc1(x)
         logits = self.out(x)
         return logits
+
+
 class BrainDQNMain(object):
     def __init__(self, actions):
         """
@@ -98,6 +100,7 @@ class BrainDQNMain(object):
         self.loss_func = nn.MSELoss()
         LR = 1e-6
         self.optimizer = torch.optim.Adam(self.Q_net.parameters(), lr=LR)
+
     def save(self):
         print("保存模型")
         torch.save(self.Q_net.state_dict(), 'params3.pth')
@@ -130,7 +133,7 @@ class BrainDQNMain(object):
         y_batch = np.zeros([BATCH_SIZE, 1])
         # shape: (8, 4, 80, 80)
         nextState_batch = np.array(nextState_batch)
-        #shape: (8, 4, 80, 80)
+        # shape: (8, 4, 80, 80)
         nextState_batch = torch.Tensor(nextState_batch)
         action_batch = np.array(action_batch)
         action_batch_tensor = torch.LongTensor(action_batch).unsqueeze(1)
@@ -146,7 +149,7 @@ class BrainDQNMain(object):
                 # 做所有动作的Q值数组，y计算为如果游戏停止，y=rewaerd[i],如果没停止，则y=reward[i]+gamma*np.max(Qvalue[i])
                 # 代表当前y值为当前reward+未来预期最大值*gamma(gamma:经验系数)
                 y_batch[i][0] = reward_batch[i] + \
-                    GAMMA * np.max(QValue_batch[i])
+                                GAMMA * np.max(QValue_batch[i])
         # y_batch: shape(batch_size, 1)
         y_batch = np.array(y_batch)
         # 确认一下形状，变成了 (batch_size, 1)
@@ -159,7 +162,7 @@ class BrainDQNMain(object):
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
-        #定期更新target网络
+        # 定期更新target网络
         if self.timeStep % UPDATE_TIME == 0:
             self.Q_netT.load_state_dict(self.Q_net.state_dict())
             self.save()
@@ -183,7 +186,7 @@ class BrainDQNMain(object):
         # 更新当前的状态， 只更新第4个层，self.currentState[1:, :, :]表示丢弃第一个层，然后append后，又变成 (4,80,80)
         newState = np.append(
             self.currentState[1:, :, :], nextObservation, axis=0)
-        #加到重放缓存中， 上一个状态self.currentState:(4,80,80), 进行的动作：action， 返回的奖励：reward，返回的状态： newState: (4,80,80)。判断游戏是否结束: terminal
+        # 加到重放缓存中， 上一个状态self.currentState:(4,80,80), 进行的动作：action， 返回的奖励：reward，返回的状态： newState: (4,80,80)。判断游戏是否结束: terminal
         self.replayMemory.append(
             (self.currentState, action, reward, newState, terminal))
         # 如果大于最大记住的buffer的数量，弹出最旧的那个
@@ -194,7 +197,7 @@ class BrainDQNMain(object):
             print(f"开始训练DQN网络")
             self.train()
 
-        #打印一些日志信息
+        # 打印一些日志信息
         if self.timeStep <= OBSERVE:
             state = "observe"
         elif self.timeStep > OBSERVE and self.timeStep <= OBSERVE + EXPLORE:
@@ -202,9 +205,9 @@ class BrainDQNMain(object):
         else:
             state = "train"
         print(f"已经进行了{self.timeStep}步，现在处理 {state}状态, 当前的EPSILON值是 {self.epsilon}")
-        #更新上一个状态self.currentState到当前的状态newState
+        # 更新上一个状态self.currentState到当前的状态newState
         self.currentState = newState
-        #时间步+1
+        # 时间步+1
         self.timeStep += 1
 
     def getAction(self):
@@ -263,7 +266,7 @@ if __name__ == '__main__':
     # 变成灰度图，尺寸变成 (400, 800, 3) --> (80, 80)
     observation0 = cv2.cvtColor(cv2.resize(
         observation0, (width, height)), cv2.COLOR_BGR2GRAY)
-    #二值化, 小于1的设为0，大于1的设为255，相当于能看到了整个图像的轮廓
+    # 二值化, 小于1的设为0，大于1的设为255，相当于能看到了整个图像的轮廓
     ret, observation0 = cv2.threshold(observation0, 1, 255, cv2.THRESH_BINARY)
     # 设置观察到的第一个游戏状态
     brain.setInitState(observation0)
@@ -272,10 +275,10 @@ if __name__ == '__main__':
         print()
         # 通过强化学习获取动作,获取输出的动作，action: int
         action = brain.getAction()
-        #根据下一个动作，返回下一个状态和奖励
+        # 根据下一个动作，返回下一个状态和奖励
         nextObservation, _, reward, terminal = game.next(action)
         # 对观察到的游戏画面进行处理，nextObservation: shape: (1, 80, 80)
         nextObservation = preprocess(nextObservation)
-        #action: int, reward: int, terminal: bool
+        # action: int, reward: int, terminal: bool
         print(f"当前选择的动作是{action}, 返回的奖励是{reward}, 是否游戏已经结束 {terminal}")
         brain.setPerception(nextObservation, action, reward, terminal)
